4ee484e7ba236642d2b56b6b23a21e63bd029307
Jenny Qian <jqian@tendenci.com>
2015-04-08 10:12:27 -0500

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name='tendenci-committees',
     author='Schipul',
     author_email='programmers@schipul.com',
-    version='6.0.1',
+    version='6.0.2',
     license='GPL3',
     description='Committees addon for Tendenci',
     long_description=longdesc,
